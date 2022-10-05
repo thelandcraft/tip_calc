2bd3ef6b3d7ca043f1a6f17bb9466766de18bbf4
vetCa <48355949+thelandcraft@users.noreply.github.com>
2022-10-06 00:06:58 +0700

    v 0.2
    Добавлены:
    1.Меню (слева)
    2.Tabs на главном экране
    3.Выпадающее меню при нажатии скидочной карты

--- main.py
+++ main.py
@@ -1,43 +1,248 @@
+#pip install kivy
+#pip install kivymd
+#pip install https://github.com/kivymd/KivyMD/archive/3274d62.zip
+
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty
 
 from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
+
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+
+from kivymd.icon_definitions import md_icons
+from kivymd.font_definitions import fonts
+
+from kivymd.uix.menu import MDDropdownMenu
+from kivy.clock import Clock
 
 
-class Test(MDApp):
+KV = '''
+#https://stackoverflow.com/questions/65698145/kivymd-tab-name-containing-icons-and-text
+# this import will prevent disappear tabs through some clicks on them)))
+#:import md_icons kivymd.icon_definitions.md_icons
+#:import fonts kivymd.font_definitions.fonts
+
+
+# Menu item in the DrawerList list.
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
+
+    IconLeftWidget:
+        id: icon
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: root.text_color
+
+
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/kivy-icon-256.png"
+
+    MDLabel:
+        text: "Калькулятор чаевых"
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: "Автор Бурьяк Данил"
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    ScrollView:
+
+        DrawerList:
+            id: md_list
+
+
+
+Screen:
+
+    MDNavigationLayout:
+
+        ScreenManager:
+
+            Screen:
+
+                BoxLayout:
+                    orientation: 'vertical'
+
+                    MDToolbar:
+                        title: "Калькулятор чаевых"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+                        md_bg_color: 0, 0, 0, 1
+                        right_action_items: [["language-python",lambda x: nav_drawer.set_state("shutdown")]]
+                    MDTabs:
+                        id: tabs
+                        on_tab_switch: app.on_tab_switch(*args)
+                        height:"48dp"
+                        tab_indicator_anim: False
+                        background_color: 0.1, 0.1, 0.1, 1
+
+                        Tab:
+                            id: tab1
+                            name: 'tab1'
+                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['calculator']}[/size][/font] Калькулятор"
+                        
+                            BoxLayout:
+                                orientation: 'vertical'
+                                padding: "10dp"   
+                                
+                                BoxLayout:
+                                    orientation: 'horizontal'                               
+                                    
+                                    MDIconButton:
+                                        icon: "account-cash"
+                                        
+                                    MDTextField:
+                                        hint_text: "Счет чека"
+                                
+                                BoxLayout:
+                                    orientation: 'horizontal'                         
+                                    
+                                    MDIconButton:
+                                        icon: "percent"
+                                        
+                                    MDTextField:
+                                        hint_text: "Процент чаевых,%"
+                                    
+                                BoxLayout:
+                                    orientation: 'horizontal'                                
+                                    
+                                    MDIconButton:
+                                        icon: "account-group"
+                                            
+                                    MDTextField:
+                                        hint_text: "Разделить счёт"
+                                    
+                                BoxLayout:
+                                    orientation: 'horizontal'                                 
+                                    
+                                    MDIconButton:
+                                        icon: "card"
+                                            
+                                    
+                                    MDTextField:
+                                        id: payment_type
+                                        hint_text: "Скидочная карта"
+                                        on_focus: if self.focus: app.menu.open()
+                        Tab:
+                            id: tab2
+                            name: 'tab2'
+                            text: f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons['table-large']}[/size][/font] Список"
+                        
+                    Widget:
+
+                
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                id: content_drawer
+'''
+
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
+class ContentNavigationDrawer(BoxLayout):
+    pass
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Вызывается при нажатии на пункт меню."""
+
+        # Установка цвета значка и текста для пункта меню.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+
+class CalculateTip(MDApp):
+
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_string(KV)
+        # https://kivymd.readthedocs.io/en/latest/components/menu/?highlight=MDDropDownItem#center-position
+        #menu_items = [{"icon": "git", "text": f"Item {i}"} for i in range(5)]
+        menu_items = [{"icon": "checkbox-marked-circle-outline", "text": "Да"},
+                      {"icon": "bolnisi-cross", "text": "Нет"}]
+        self.menu = MDDropdownMenu(
+            caller=self.screen.ids.payment_type,
+            items=menu_items,
+            position="auto",
+            width_mult=4,
+        )
+        self.menu.bind(on_release=self.set_item)
+
+    def set_item(self, instance_menu, instance_menu_item):
+        def set_item(interval):
+            self.screen.ids.payment_type.text = instance_menu_item.text
+            instance_menu.dismiss()
+
+        Clock.schedule_once(set_item, 0.5)
 
     def build(self):
-        self.theme_cls.material_style = "M3"
-        self.theme_cls.theme_style = "Dark"
-        return Builder.load_string(
-            '''
-MDScreen:
+        # self.theme_cls.theme_style = "Light"  # "Dark"  # "Light"
+        # return Builder.load_string(KV)
+        return self.screen
 
-    MDBottomNavigation:
-        #panel_color: "#eeeaea"
-        selected_color_background: "green"
-        text_color_active: "lightgrey"
-
-        MDBottomNavigationItem:
-            name: 'screen 1'
-            text: 'Калькулятор'
-            icon: 'calculator'
-            
-
-            MDLabel:
-                text: 'About me'
-                halign: 'center'
+    
 
 
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'About Me'
-            icon: 'pencil'
+    def on_start(self):
+        icons_item = {
+            "book":"О приложении",
+            "github":"Код на GitHub",
+            "alert":"Нашли ошибку",
+        }
+        icons_item_tab = {
+            "alert":"Нашли ошибку",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
 
-            MDLabel:
-                text: 'Связь со мной : +7(999)999-99-99'
-                halign: 'center'
-'''
-        )
+        # for name_tab in list(md_icons.keys())[15:30]:
+        #     self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab)
+        #     )
 
 
-Test().run()
+        # for icon_name, name_tab in icons_item_tab.items():
+        #     self.root.ids.tabs.add_widget(
+        #          Tab(
+        #              text=f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/size][/font] {name_tab}"
+        #             )
+        #         )
+    def on_tab_switch(
+        self, instance_tabs, instance_tab, instance_tab_label, tab_text
+        ): 
+        print("click" + tab_text)
+      
+
+        
+CalculateTip().run()
